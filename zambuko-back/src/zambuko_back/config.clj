064456bbc29c5cc148a792204632a3a981cc17cb
Eduardo Diaz <iamedu@Gmail.com>
2013-08-11 09:50:13 -0500

    All services are up

--- zambuko-back/src/zambuko_back/config.clj
+++ zambuko-back/src/zambuko_back/config.clj
@@ -2,10 +2,16 @@
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
             [org.jclouds.blobstore2 :as bs]
-            [monger.core :as mc]))
+            [monger.core :as mc]
+            [clojure.tools.nrepl.server :as nrepl]
+            [hornetq-clj.server :as server]
+            [hornetq-clj.core-client :as client]))
 
 (def zambuko-config (atom nil))
 (def bstore (atom nil))
+(def nrepl-server (atom nil))
+(def hornet-server (atom nil))
+(def hornet-session-factory (atom nil))
 
 (defn setup-system! []
   (let [props (:system-properties @zambuko-config {})]
@@ -22,8 +28,23 @@
     (mc/use-db! db)
     (mc/authenticate (mc/get-db db) username (.toCharArray password))))
 
+(defn start-embedded-hornetq! [zc]
+  (let [server (server/server {:in-vm true})]
+    (reset! hornet-server server)
+    (.start server)
+    (client/in-vm-session-factory zc)))
+
 (defn start-hornetq! []
-  (let [{:keys [embedded]} (:hornetq @zambuko-config)]))
+  (let [{:keys [embedded] :as zc} (:hornetq @zambuko-config)]
+    (reset! hornet-session-factory
+            (if embedded
+              (start-embedded-hornetq! zc)
+              (client/netty-session-factory zc)))))
+
+(defn start-nrepl! []
+  (let [{:keys [start port]} (:nrepl @zambuko-config)]
+    (when start 
+      (reset! nrepl-server (nrepl/start-server :port port)))))
 
 (defn load-config []
   (with-open [stream (java.io.PushbackReader. (io/reader "server.edn"))]
@@ -37,5 +58,6 @@
   (setup-system!)
   (start-blobstore!)
   (start-mongo!)
-  (start-hornetq!))
+  (start-hornetq!)
+  (start-nrepl!))
 
